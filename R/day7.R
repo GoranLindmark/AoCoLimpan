c53cc0d634144af433573294c85cc4d840fb5ad3
Göran Lindmark <lindmark.goran@gmail.com>
2022-12-15 17:02:44 +0100

    day7, completed

--- R/day7.R
+++ R/day7.R
@@ -1,17 +1,12 @@
-day7
-library(tidyverse)
-library(r2r)
-library(stringr)
-library(readr)
 
+#' day7
+#'
+#' @return
+#' @export
+#' @import from magrittr %>%
+#' @import dplyr, tidyr, stringr, readr
 
-
-
-day7 <- function(){
-
-
-
-
+day7 <- function() {
   findStructure <- function() {
     parser <- function(cmdStr) {
       # browser()
@@ -29,18 +24,26 @@ day7 <- function(){
       if (stringr::str_detect(cmdStr, '\\$ dir')) {
         res <-
           list(case = "ls",
-               value = str_sub(cmdStr, start = 6, end = nchar(cmdStr)))
+               value = stringr::str_sub(cmdStr, start = 6, end = nchar(cmdStr)))
       }
 
       if (stringr::str_detect(cmdStr, '[[:digit:]]')) {
         # res <- list(case = "file", value = str_sub(cmdStr, start = 1, end = 6 ))
-        res <- list(case = "file", value = as.numeric(parse_number(cmdStr)))
+
+
+        res <-
+          list(case = "file",
+               value = as.numeric(readr::parse_number(cmdStr, '[[:digit:]]' )),
+               name = str_sub(cmdStr,
+                              start = str_locate(cmdStr , "[[:alpha:]]" )[1],
+                              end = nchar(cmdStr))
+               )
       }
 
       if (stringr::str_sub(cmdStr, start = 1L, end = 3) == 'dir') {
         res <-
           list(case = "dir",
-               value = str_sub(cmdStr, start = 5, end = -1L))
+               value = stringr::str_sub(cmdStr, start = 5, end = -1L))
       }
 
       if (stringr::str_detect(cmdStr, '\\$ cd \\.\\.')) {
@@ -50,69 +53,65 @@ day7 <- function(){
       return(res)
     }
 
-    cmdLogg <- read.csv("data-raw/day7rawDataSmall", header = F)
+    cmdLogg <- read.csv("data-raw/day7rawData", header = F)
 
-    dirrrs <- character()
-    subdirr <- character()
-    size <- double()
-    tree <- data.frame(dirrrs, subdirr, size)
+    dirStructure <- tidyr::tibble( dir = character(), sDir = character())
+    fileStructure <- tidyr::tibble ( fName = character(), dir = character(), size = double())
 
-    row <- 1  # row in command log
-    dirName <- NA
-    subdir <- NA
-    ls <- F
 
-    outRow <- 1
-    dirName <- subdir <- "root"
+    dir <- subDir <- "root"
+    row <- 1
 
     repeat {
 
-
       res <- parser(cmdLogg$V1[row])
-      print(paste(res$case, "-", res$value))
-
-      if (res$case == "cd") {
-
-        subdir <- dirName
-        dirName <= as.character(res$value)
-        ls <- F
+      # print(paste(res$case, "-", res$value))
+      if ( res$case == "cd" ){
+              if (res$value != ".."){
+                dir <- res$value
+              }
       }
 
-      if (res$case == "dir") {
-
+      if ( res$case == "dir"){
+        subDir <- res$value
+        dirStructure <- rbind( dirStructure, tibble(dir = dir, sDir = subDir))
       }
 
-      if (res$case == "ls") {
-        ls <- T
-      }
       if (res$case == "file") {
-        browser()
-        tibble(dirrrs = dirname,
-                   subdirr = subdir,
-                   size = res$value)
-        tree <- data.frame(dirrrs = dirname,
-                   subdirr = subdir,
-                   size = res$value)
-        tree <- rbind(tree, data.frame(dirrrs = dirname,
-                                               subdirr = subdir,
-                                               size = res$value))
-        outRow <- outRow + 1
+        fileStructure <- rbind(fileStructure, tibble(
+          fName = res$name, dir = dir, size = res$value))
       }
-
-
 
 
       if (row == nrow(cmdLogg)) {
-        return(tree)
         break
       } else {
         row <- row + 1
       }
     }
-
+    return( list( dirStructure = dirStructure, fileStructure = fileStructure)  )
   }
-  findStructure()
-  return(tree)
+
+  # ------ MAIN
+
+  res <-findStructure()
+
+  fileStructure <- tidyr::as_tibble(res$fileStructure )
+  dirStructure <- tidyr::as_tibble( res$dirStructure )
+
+  fileStructure <-
+    fileStructure %>%
+    dplyr::group_by(dir) %>%
+    dplyr::summarize(totSize = sum(size))
+
+  allDirs <- tibble(all = unique(c(as.character(dirStructure$dir),
+                      as.character(dirStructure$sDir))))
+
+  res <-
+    left_join(dirStructure, fileStructure, by = c("sDir" = "dir")) %>%
+    group_by(dir) %>%
+    summarize (totSize = sum(totSize, na.rm = T)) %>%
+    filter(totSize < 100000)
+  return(res)
 }
 day7()
-
